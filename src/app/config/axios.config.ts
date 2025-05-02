389bab0b402b134bcf8b782a0a10d9080c023ac6
Thelry D. Bissi <thelry.d.b@hotmail.com>
2025-05-02 17:34:33 -0300

    Update urlBase for backend

--- src/app/config/axios.config.ts
+++ src/app/config/axios.config.ts
@@ -2,18 +2,17 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api', // Sua URL base
+    baseURL: 'https://brazilian-universities-backend-production.up.railway.app/api/Universities', 
     timeout: 10000,
     headers: {
       'Content-Type': 'application/json'
     }
   });
   
-  // Configuração de interceptors global
+  
   axiosInstance.interceptors.response.use(
-    response => response.data, // Retorna apenas os dados
+    response => response.data, 
     error => {
-      // Tratamento global de erros
       console.error('Axios error:', error);
       return Promise.reject(error);
     }
